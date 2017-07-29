e3cdb5e78f5b6580f924f5c2ce64c534a96d6b29
Hack Reactor Student <student@hackreactor.com>
2017-07-28 20:08:19 -0700

    Create messages, users, rooms tables in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,24 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer auto_increment primary key,
+  user_id integer,
+  content text not null,
+  room_id integer,
+  foreign key(user_id) references users(id),
+  foreign key(room_id) references rooms(id),
+);
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id integer auto_increment primary key,
+  name text not null,
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id integer auto_increment primary key,
+  roomname text not null,
 );
 
 /* Create other tables and define schemas for them here! */
